edce49107330a2f5777354e2fc53c8f8edb80efa
chris-malloy <chrismalloymusic@gmail.com>
2018-03-15 14:50:51 -0400

    data pieces laid out properly

--- src/components/WeatherCard.js
+++ src/components/WeatherCard.js
@@ -1,14 +1,31 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { CardPanel, Row, Col } from 'react-materialize';
 
 const WeatherCard = (props) => {
   return (
       <div>
-        {props.temp}
-        {props.icon}
-        {props.date}
-        {props.high}
-        {props.low}
+        <CardPanel>
+          <Row>
+            <Col s={6}>
+              {props.temp}°
+            </Col>
+            <Col s={6}>
+              {props.icon}
+            </Col>
+            <Col s={6}>
+            {props.date}
+            </Col>
+            <Col s={6}>
+              <Col s={12}>
+              {props.high}°
+              </Col>
+              <Col s={12}>
+                {props.low}°
+              </Col>
+            </Col>
+          </Row>
+        </CardPanel>
       </div>
   );
 };
